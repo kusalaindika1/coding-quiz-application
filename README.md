f8db763964cc5fc9ca3615578fb780d3b845aa10
ethugalge liyanage <mycanada123@gmail.com>
2022-07-29 20:11:19 -0400

    add index.html

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # coding-quiz-application
+
+
+# description
+
+This web page is about a coding qize application. this web application has ten questions and maltiple choices with 30 minute time zone. you can choose one answer and end of the ten question you will recive your points.
+
+
+
+
+# screenshot of my application
+
+
+![quiz!](images/pic123.png)
+
+
+
+
+# github repository link
+
+
+
+
+
+# github deploy link
---
+++ js/questions.js
@@ -0,0 +1,120 @@
+// creating  questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+       numb: 6,
+      question:  "A program in HTML can be rendered and read by -",
+      answer: "Web browser",
+       options: [
+       "Web browser",
+        "new line",
+        "markup langage",
+         "server"
+      ]
+     },  
+
+
+     {
+        numb: 7,
+        question: "What does XML stand for?",
+        answer: "horizontal ruler",
+        options: [
+          "new line",
+          "eXecutable Multiple Language",
+          "horizontal ruler",
+          "eXamine Multiple Language"
+        ]
+      },
+
+      {
+        numb: 8,
+        question: "The <hr> tag in HTML is used for -",
+        answer: "eXtensible Markup Language",
+        options: [
+          "eXtensible Markup Language",
+          "eXecutable Multiple Language",
+          "eXTra Multi-Program Language",
+          "eXamine Multiple Language"
+        ]
+      }, 
+
+    
+      {
+        numb: 9,
+        question: "What does XML stand for?",
+        answer: "eXtensible Markup Language",
+        options: [
+          "eXtensible Markup Language",
+          "eXecutable Multiple Language",
+          "eXTra Multi-Program Language",
+          "eXamine Multiple Language"
+        ]
+      },
+
+      {
+        numb: 10,
+        question: "What does XML stand for?",
+        answer: "eXtensible Markup Language",
+        options: [
+          "eXtensible Markup Language",
+          "eXecutable Multiple Language",
+          "eXTra Multi-Program Language",
+          "eXamine Multiple Language"
+        ]
+      },
+    
+    ]
